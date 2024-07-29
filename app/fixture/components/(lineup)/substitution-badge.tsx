579b664ac15e1d4557dc2bc3f9cc085f304c9e32
Nick Ng <nickng@Nicks-MacBook-Pro.local>
2024-07-29 10:38:33 +0800

    refactor: modify substitution badge display for goalkeeper

--- app/fixture/components/(lineup)/substitution-badge.tsx
+++ app/fixture/components/(lineup)/substitution-badge.tsx
@@ -6,14 +6,21 @@ import Badge from '@/app/fixture/components/(lineup)/badge'
 interface Props {
     type: 'in' | 'out'
     value: number
+    isGoalkeeper: boolean
 }
 
-export default function SubstitutionBadge({ type, value }: Props) {
+export default function SubstitutionBadge({
+    type,
+    value,
+    isGoalkeeper,
+}: Props) {
     return (
         <View
             className={clsx('absolute items-center', {
-                '-left-[22px] -top-[2.5px] flex-row': type === 'in',
-                '-left-1 -top-[15px] justify-center': type === 'out',
+                '-left-[22px] -top-[2.5px] flex-row':
+                    type === 'in' || (type === 'out' && isGoalkeeper),
+                '-left-1 -top-[15px] justify-center':
+                    type === 'out' && !isGoalkeeper,
             })}
         >
             <Text
